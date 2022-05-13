04ac15ebd1179b0a622ade1c8b48cc3dc3f6ff0d
Ronaldo Cysne <ronaldocysne@gmail.com>
2022-05-13 17:13:32 -0300

    Instalação de pacotes e conserto do JS

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const nunjucks = require('nunjucks');
 const server = express();
+
+//Qual a engine usar e qual o tipo de arquivo
+server.set("view engine", "html"); 
+
+//Local dos arquivos a serem lidos, na pasta views e utilizando o express e qual o arquivo que ele está olhando, no caso o server que está definindo a engine e o tipo de arquivo
+nunjucks.configure("views", { 
+  express:server
+})
 
 
 server.get('/', (req, res) => res.send('Dev Ronaldo Cysne'));
-
-
-
 
 
 
