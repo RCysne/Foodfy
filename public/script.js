099edad6c22c9caf814867e55cb873a9bdad436d
Ronaldo Cysne <cysne@pop-os.localdomain>
2022-10-04 16:18:27 -0300

    Modais ok

--- public/script.js
+++ public/script.js
@@ -4,12 +4,9 @@ const pratos = document.querySelectorAll('.pratoR');
 for (let pratoR of pratos) {
   pratoR.addEventListener('click', () => {
     const imageId = pratoR.getAttribute('id'); // A cada prato, pegue a id do mesmo
-    // const h3 = '';
-    // const p = '';
+
     modalOverlay.classList.add('active');
     modalOverlay.querySelector('img').src = `/public/images/${imageId}.png`;
-    // modalOverlay.querySelector('.nomePrato'). = 
-    // modalOverlay.querySelector('.nomeChef'). =
   });
 }
 
